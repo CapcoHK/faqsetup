041312b72ad05ce21e060813b9b05d10c7ace4f0
capco-bhushan <bhushan.dravid@capco.com>
2017-07-11 15:53:43 +0800

    Redirected extract_setup.sh output to log file

--- scripts/extract_setup.sh
+++ scripts/extract_setup.sh
@@ -1,9 +1,7 @@
+#!/usr/bin/env bash
+cd ~/Downloads/slack/taming-text/faqsetup/
+
 echo "Extracting setup from excel file Slack_FAQ.xlsx"
-java -cp faqsetup.jar com.capco.slack.faq.setup.output.TextFileSetupExporter "Slack_FAQ.xlsx" "../book/apache-solr/solr-qa/conf/synonyms.txt" "../book/apache-solr/solr-qa/conf/stopwords.txt" "../book/bin/faq.txt"
+java -cp faqsetup.jar com.capco.slack.faq.setup.output.TextFileSetupExporter "Slack_FAQ.xlsx" "../book/apache-solr/solr-qa/conf/synonyms.txt" "../book/apache-solr/solr-qa/conf/stopwords.txt" "../book/bin/faq.txt" &> ~/Downloads/slack/logs/setup-exporter.log
 
-cd ../book/bin/
-
-
-echo "Index new faq questions..."
-sh indexWikipedia.sh --wikiFile faq.txt
-echo "Finished executing script.. "
+cd ~
